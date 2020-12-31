26164a2c069ebc4c0956f4177a105043aec31c16
Aaron <aarongokani@hotmail.com>
2020-12-31 18:02:15 +0000

    Adds text animation (fade-reveal) to About Page

--- src/Components/ProfileIntro/ProfileIntro.js
+++ src/Components/ProfileIntro/ProfileIntro.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import { useSprings, animated, useTransition } from "react-spring";
+import { Transition } from "react-spring/renderprops";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 import fox from "./fox.png";
 
 export const ProfileIntro = () => {
+  // console.log(letterObject);
+
   return (
     <div>
       <img
@@ -16,13 +21,18 @@ export const ProfileIntro = () => {
       />
       <div style={{ display: "flex", flexDirection: "column" }}>
         <h3 className='profile-headline'>Hi, I'm Aaron</h3>
+
         <p className='profile-description'>
-          I am a full-stack software developer. Feel free to take a look at my
-          latest projects on my{" "}
-          <a href='/portfolio' style={{ color: "#cacaca", fontWeight: "bold" }}>
-            portfolio page
-          </a>
-          . Remotely available UTC−1 to UTC+8. aarongokani@gmail.com
+          <Fade right cascade delay={1500}>
+            I am a full-stack software developer. Feel free to take a look at my
+            latest projects on my{" "}
+            <a
+              href='/portfolio'
+              style={{ color: "#cacaca", fontWeight: "bold" }}
+            >
+              portfolio page
+            </a>
+          </Fade>
         </p>
       </div>
     </div>
